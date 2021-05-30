13792055ed17b38598f2a079ec79d1543b362c80
Fattakhov26 <16.05.123456@mail.ru>
2021-05-30 19:39:44 +0500

    Add scripts for PDF generation

--- www/js/calculate.js
+++ www/js/calculate.js
@@ -84,21 +84,216 @@ function updateTrialModeForms(){
 		$('.result__trial__mode').html(result);
 	}
 }
+function createPdfIfSingleMode(tokenCookie){
+
+	let ratesId = $(".rates__selector").val();
+
+	let firstMeterPrevReadings = $(".prev__single__rate__price").val();
+	let firstMeterCurrReadings = $(".curr__single__rate__price").val();
+	let consumptionByFirstMeter = firstMeterCurrReadings - firstMeterPrevReadings;
+	let firstMeterAmount = $(".result__single__mode").html();
+	let totalAmount = $(".result__single__mode").html();
+
+	$.ajax({
+		url: '/api/generate_pdf',     
+		type: 'GET',
+		data : {
+            //token : tokenCookie,
+            meterMode : 1,
+            ratesId : ratesId,
+            firstMeterPrevReadings : firstMeterPrevReadings,
+            firstMeterCurrReadings : firstMeterCurrReadings,
+            consumptionByFirstMeter : consumptionByFirstMeter,
+            firstMeterAmount : firstMeterAmount,
+            totalAmount : totalAmount,
+        },
+        dataType: 'json',                   
+        success: function(data)
+        {
+        	let pdfDocDownloadLink = "https://project26.usatu.su" + data.responseBody;
+        	alert("Отчёт сформирован. Нажмите ОК для просмотра");
+        	window.open(pdfDocDownloadLink, '_blank');
+        },
+        statusCode: {
+        	400: function (response) {
+        		alert("Недопустимые данные");
+        	},
+        	500: function (response) {
+        		alert("Произошла ошибка");
+        	}
+        },
+    })
+
+}
+function createPdfIfDualMode(tokenCookie){
+
+	let ratesId = $(".rates__selector").val();
+
+	let firstMeterPrevReadings = $(".prev__daily__rate__price").val();
+	let firstMeterCurrReadings = $(".curr__daily__rate__price").val();
+	let secondMeterPrevReadings = $(".prev__night__rate__price").val();
+	let secondMeterCurrReadings = $(".curr__night__rate__price").val();
+	let consumptionByFirstMeter = roundNumber(firstMeterCurrReadings - firstMeterPrevReadings, 2);
+	let consumptionBySecondMeter = roundNumber(secondMeterCurrReadings - secondMeterPrevReadings, 2);
+	
+	let dailyRatePrice = parseFloat($('.daily__rate__price').html());
+	let nightRatePrice = parseFloat($('.night__rate__price').html());
+	
+	let firstMeterAmount = roundNumber(consumptionByFirstMeter * dailyRatePrice, 2);
+	let secondMeterAmount = roundNumber(consumptionBySecondMeter * nightRatePrice, 2);
+	let totalAmount = roundNumber(firstMeterAmount + secondMeterAmount, 2);
+	
+
+	$.ajax({
+		url: '/api/generate_pdf',     
+		type: 'GET',
+		data : {
+            //token : tokenCookie,
+            meterMode : 2,
+            ratesId : ratesId,
+            firstMeterPrevReadings : firstMeterPrevReadings,
+            firstMeterCurrReadings : firstMeterCurrReadings,
+            consumptionByFirstMeter : consumptionByFirstMeter,
+            firstMeterAmount : firstMeterAmount,
+            totalAmount : totalAmount,
+            secondMeterPrevReadings : secondMeterPrevReadings,
+			secondMeterCurrReadings : secondMeterCurrReadings,
+			consumptionBySecondMeter : consumptionBySecondMeter,
+			secondMeterAmount : secondMeterAmount
+        },
+        dataType: 'json',                   
+        success: function(data)
+        {
+        	let pdfDocDownloadLink = "https://project26.usatu.su" + data.responseBody;
+        	alert("Отчёт сформирован. Нажмите ОК для просмотра");
+        	window.open(pdfDocDownloadLink, '_blank');
+        },
+        statusCode: {
+        	400: function (response) {
+        		alert("Недопустимые данные");
+        	},
+        	500: function (response) {
+        		alert("Произошла ошибка");
+        	}
+        },
+    })
+
+}
+function createPdfIfTrialMode(tokenCookie){
+
+	let ratesId = $(".rates__selector").val();
+
+	let firstMeterPrevReadings = $(".prev__peak__zone__rate__price").val();
+	let firstMeterCurrReadings = $(".curr__peak__zone__rate__price").val();
+	let secondMeterPrevReadings = $(".prev__semipeak__zone__rate__price").val();
+	let secondMeterCurrReadings = $(".curr__semipeak__zone__rate__price").val();
+	let thirdMeterPrevReadings = $(".prev__night__zone__rate__price").val();
+	let thirdMeterCurrReadings = $(".curr__night__zone__rate__price").val();
+	let consumptionByFirstMeter = roundNumber(firstMeterCurrReadings - firstMeterPrevReadings, 2);
+	let consumptionBySecondMeter = roundNumber(secondMeterCurrReadings - secondMeterPrevReadings, 2);
+	let consumptionByThirdMeter = roundNumber(thirdMeterCurrReadings - thirdMeterPrevReadings, 2);
+	
+	let peakZoneRatePrice = parseFloat($('.peak__zone__rate__price').html());
+	let semipeakZoneRatePrice = parseFloat($('.semipeak__zone__rate__price').html());
+	let nightZoneRatePrice = parseFloat($('.night__zone__rate__price').html());
+	
+	let firstMeterAmount = roundNumber(consumptionByFirstMeter * peakZoneRatePrice, 2);
+	let secondMeterAmount = roundNumber(consumptionBySecondMeter * semipeakZoneRatePrice, 2);
+	let thirdMeterAmount = roundNumber(consumptionByThirdMeter * nightZoneRatePrice, 2);
+	let totalAmount = roundNumber(firstMeterAmount + secondMeterAmount + thirdMeterAmount, 2)
+
+	$.ajax({
+		url: '/api/generate_pdf',     
+		type: 'GET',
+		data : {
+            //token : tokenCookie,
+            meterMode : 3,
+            ratesId : ratesId,
+            firstMeterPrevReadings : firstMeterPrevReadings,
+            firstMeterCurrReadings : firstMeterCurrReadings,
+            consumptionByFirstMeter : consumptionByFirstMeter,
+            firstMeterAmount : firstMeterAmount,
+            totalAmount : totalAmount,
+            secondMeterPrevReadings : secondMeterPrevReadings,
+			secondMeterCurrReadings : secondMeterCurrReadings,
+			consumptionBySecondMeter : consumptionBySecondMeter,
+			secondMeterAmount : secondMeterAmount,
+			thirdMeterPrevReadings : thirdMeterPrevReadings,
+			thirdMeterCurrReadings : thirdMeterCurrReadings,
+			consumptionByThirdMeter: consumptionByThirdMeter,
+			thirdMeterAmount : thirdMeterAmount
+        },
+        dataType: 'json',                   
+        success: function(data)
+        {
+        	let pdfDocDownloadLink = "https://project26.usatu.su" + data.responseBody;
+        	alert("Отчёт сформирован. Нажмите ОК для просмотра");
+        	window.open(pdfDocDownloadLink, '_blank');
+        },
+        statusCode: {
+        	400: function (response) {
+        		alert("Недопустимые данные");
+        	},
+        	500: function (response) {
+        		alert("Произошла ошибка");
+        	}
+        },
+    })
+
+}
+//очистка форм
+$('.calculation__form').find("select").val("0");
+$('.calculation__form').find("input").val('');
 
 //слушатели (обновление результатов при взаимодействии):
-//выпадающее меню
+//выпадающее меню выбора тарифных ставок
 $('.rates__selector').change(function() {
 	writeRatesToHiddenTags();
 
 });
-//кнопка рассчёта
+//выпадающее меню выбора типа счётчика
+$('.meter__mode__selector').change(function() {
+	if ($(this).find(":selected").val() == 1) {
+		$('.single__rates__div').show();
+		$('.dual__rates__div').hide();
+		$('.trial__rates__div').hide();
+	}
+	if ($(this).find(":selected").val() == 2) {
+		$('.single__rates__div').hide();
+		$('.dual__rates__div').show();
+		$('.trial__rates__div').hide();
+	} 
+	if ($(this).find(":selected").val() == 3) {
+		$('.single__rates__div').hide();
+		$('.dual__rates__div').hide();
+		$('.trial__rates__div').show();
+	}
+});
+let successCalculation = false;
+//кнопка расчёта
 $('.calculate__button').click(function() {
 	let ratesId = $('.rates__selector').val();
-	if (ratesId == 0){
-		alert ("Выберите подходящую категорию тарифов")
+	let meterMode = $('.meter__mode__selector').val();
+	if (ratesId == 0 || meterMode == 0){
+		alert ("Выберите подходящую категорию тарифов и режим работы счётчика")
 	} else {
 		updateSingleModeForms();
 		updateDualModeForms();
 		updateTrialModeForms();
+		successCalculation = true;
+	}
+});
+//кнопка PDF отчёта
+$('.report__button').click(function() {
+	let ratesId = $('.rates__selector').val();
+	let meterMode = $('.meter__mode__selector').val();
+	if (successCalculation && meterMode == 1){
+		createPdfIfSingleMode();
+	} else if (successCalculation && meterMode == 2){
+		createPdfIfDualMode();
+	}  else if (successCalculation && meterMode == 3){
+		createPdfIfTrialMode();
+	} else {
+		alert ("Сначала нужно произвести расчёт");
 	}
 });
